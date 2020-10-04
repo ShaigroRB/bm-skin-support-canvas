e11ebb3ded8b13a25ba98aa6dd7909dbf62c0e7f
ShaigroRB <robin.boucher@epita.fr>
2020-10-04 02:09:58 +0200

    add SpriteSettings + remove using all global variables from functions
    - add regions
    - moved classes before variables because of initialization problems
    - rename slieCanvas to sliceSprite
    - refactor functions so it use parameters instead of global variables

--- canvas.js
+++ canvas.js
@@ -1,42 +1,45 @@
-// --------------- Variables -------------
-// define zoom factor
-const RESIZE_FACTOR = 15;
-
-// ids of elements
-const ID_CANVAS_ORIGINAL_SPRITE = "canvas-original-sprite";
-const ID_CANVAS_DEF_WEAP = "canvas-default-weapon";
-const ID_CANVAS_TMP = "canvas-tmp";
-const ID_CANVAS_MODIF_PATTERN = "canvas-modif-pattern";
-const ID_INPUT_SPRITE_WIDTH = "input-sprite-width";
-const ID_INPUT_SPRITE_HEIGHT = "input-sprite-height";
-const ID_INPUT_NB_PATTERNS = "input-number-patterns";
-const ID_INPUT_IMG = "input-img";
-const ID_BTN_START_MODIF = "btn-start-modification";
-const ID_BTN_SLICE = "btn-slice";
-const ID_BTN_ZOOM_IN = "btn-zoom-in";
-const ID_BTN_ZOOM_OUT = "btn-zoom-out";
-const ID_BTN_LIGHTEST = "btn-lightest-color";
-const ID_BTN_NORMAL = "btn-normal-color";
-const ID_BTN_DARKEST = "btn-darkest-color";
-const ID_BTN_ERASE = "btn-erase-color";
-const ID_BTN_CLEAR = "btn-clear";
-const ID_DIV_MODIF = "div-modification";
-const ID_DIV_ZOOM_IN = "div-zoom-in";
-const ID_DIV_ZOOM_OUT = "div-zoom-out";
-
-let widthWeapon = 96;
-let heightWeapon = 64;
-let nbPatterns = 14;
-
-let originalCanvas = document.getElementById(ID_CANVAS_ORIGINAL_SPRITE);
-let pattern = 1;
-let currPixelColor = new PixelColor();
-
 // -------------- Classes -----------------
+//#region 
+/**
+ * Represents the settings of a sprite
+ */
+class SpriteSettings {
+    /**
+     * @param {HTMLCanvasElement} spriteCanvas 
+     * @param {number} widthPattern 
+     * @param {number} heightPattern 
+     * @param {number} nbPatterns 
+     */
+    constructor(spriteCanvas, widthPattern, heightPattern, nbPatterns) {
+        this.canvas = spriteCanvas;
+        this.widthPattern = widthPattern;
+        this.heightPattern = heightPattern;
+        this.nbPatterns = nbPatterns;
+    }
+
+    setWidthPattern = (width) => {
+        this.widthPattern = width;
+    }
+
+    setHeightPattern = (height) => {
+        this.heightPattern = height;
+    }
+
+    setNbPatterns = (nb) => {
+        this.nbPatterns = nb;
+    }
+}
+
 /**
  * Represents the color of a pixel
  */
 class PixelColor {
+    /**
+     * @param {number} red 
+     * @param {number} green 
+     * @param {number} blue 
+     * @param {number} alpha 
+     */
     constructor(red, green, blue, alpha) {
         this.r = red;
         this.g = green;
@@ -68,23 +71,68 @@ class PixelColor {
     }
 }
 
+//#endregion
+
+// --------------- Variables -------------
+//#region 
+// define zoom factor
+const RESIZE_FACTOR = 15;
+
+// ids of elements
+const ID_CANVAS_ORIGINAL_SPRITE = "canvas-original-sprite";
+const ID_CANVAS_DEF_WEAP = "canvas-default-weapon";
+const ID_CANVAS_TMP = "canvas-tmp";
+const ID_CANVAS_MODIF_PATTERN = "canvas-modif-pattern";
+const ID_INPUT_SPRITE_WIDTH = "input-sprite-width";
+const ID_INPUT_SPRITE_HEIGHT = "input-sprite-height";
+const ID_INPUT_NB_PATTERNS = "input-number-patterns";
+const ID_INPUT_IMG = "input-img";
+const ID_BTN_START_MODIF = "btn-start-modification";
+const ID_BTN_SLICE = "btn-slice";
+const ID_BTN_ZOOM_IN = "btn-zoom-in";
+const ID_BTN_ZOOM_OUT = "btn-zoom-out";
+const ID_BTN_LIGHTEST = "btn-lightest-color";
+const ID_BTN_NORMAL = "btn-normal-color";
+const ID_BTN_DARKEST = "btn-darkest-color";
+const ID_BTN_ERASE = "btn-erase-color";
+const ID_BTN_CLEAR = "btn-clear";
+const ID_DIV_MODIF = "div-modification";
+const ID_DIV_ZOOM_IN = "div-zoom-in";
+const ID_DIV_ZOOM_OUT = "div-zoom-out";
+
+// canvases
+const originalCanvas = document.getElementById(ID_CANVAS_ORIGINAL_SPRITE);
+const defWeaponCanvas = document.getElementById(ID_CANVAS_DEF_WEAP);
+const tmpCanvas = document.getElementById(ID_CANVAS_TMP);
+const modifPatternCanvas = document.getElementById(ID_CANVAS_MODIF_PATTERN);
+
+let pattern = 1;
+
+const currPixelColor = new PixelColor(0, 0, 0, 0);
+const defSpriteSettings = new SpriteSettings(originalCanvas, 96, 64, 14);
+
+//#endregion
+
 // ------------- Functions ---------------
+//#region
 
 /**
  * Handler for when an image is loaded
  * @param {HTMLImageElement} img 
+ * @param {SpriteSettings} defSpriteSettings
  */
-const handleImgLoad = (img) => {
-    heightWeapon = img.height;
-    draw(img);
+const handleImgLoad = (img, defSpriteSettings) => {
+    defSpriteSettings.heightPattern = img.height;
+    draw(img, defSpriteSettings);
 };
 
 /**
  * Draw an image on a canvas given its id
  * @param {HTMLImageElement} img
+ * @param {SpriteSettings} defSpriteSettings
  */
-const draw = (img) => {
-    let canvas = originalCanvas;
+const draw = (img, defSpriteSettings) => {
+    let canvas = defSpriteSettings.canvas;
     canvas.width = img.width;
     canvas.height = img.height;
     const context = canvas.getContext("2d");
@@ -125,14 +173,24 @@ const createCanvas = (canvasId, parentId) => {
  */
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const sliceCanvasGivenPattern = (canvasId, pattern) => {
-    let canvas = document.createElement("canvas");
-    canvas = document.getElementById(canvasId);
-    canvas.width = widthWeapon;
-    canvas.height = heightWeapon;
+/**
+ * Slice a pattern from a sprite to a canvas using the settings of the sprite
+ * @param {SpriteSettings} defSpriteSettings
+ * @param {HTMLCanvasElement} toCanvas
+ * @param {number} pattern
+ */
+const sliceSpriteGivenPattern = (defSpriteSettings, toCanvas, pattern) => {
+    toCanvas.width = defSpriteSettings.widthPattern;
+    toCanvas.height = defSpriteSettings.heightPattern;
 
-    const context = canvas.getContext("2d");
-    context.drawImage(originalCanvas, widthWeapon * pattern, 0, widthWeapon, heightWeapon, 0, 0, widthWeapon, heightWeapon);
+    const context = toCanvas.getContext("2d");
+    context.drawImage(
+        defSpriteSettings.canvas,
+        defSpriteSettings.widthPattern * pattern, 0,
+        defSpriteSettings.widthPattern, defSpriteSettings.heightPattern,
+        0, 0,
+        defSpriteSettings.widthPattern, defSpriteSettings.heightPattern
+    );
 };
 
 /**
@@ -305,17 +363,18 @@ const drawPixel = (canvasId, pixelX, pixelY, color) => {
     context.putImageData(imageData, 0, 0);
 };
 
-// --------------- Callbacks ----------------------
-
-callbackOnchangeForElm(ID_INPUT_SPRITE_WIDTH, (val) => { widthWeapon = val });
-callbackOnchangeForElm(ID_INPUT_SPRITE_HEIGHT, (val) => { heightWeapon = val });
-callbackOnchangeForElm(ID_INPUT_NB_PATTERNS, (val) => { nbPatterns = val });
+//#endregion
 
 // ----------------- Listeners --------------------
+//#region
+
+callbackOnchangeForElm(ID_INPUT_SPRITE_WIDTH, (val) => { defSpriteSettings.setWidthPattern(val); });
+callbackOnchangeForElm(ID_INPUT_SPRITE_HEIGHT, (val) => { defSpriteSettings.setHeightPattern(val); });
+callbackOnchangeForElm(ID_INPUT_NB_PATTERNS, (val) => { defSpriteSettings.setNbPatterns(val); });
 
 document.getElementById(ID_INPUT_IMG).onchange = (evt) => {
     let img = new Image();
-    img.onload = () => handleImgLoad(img);
+    img.onload = () => handleImgLoad(img, defSpriteSettings);
     img.onerror = () => failed();
     img.src = URL.createObjectURL(evt.target.files[0]);
 };
@@ -324,7 +383,7 @@ document.getElementById(ID_BTN_SLICE).onclick = () => {
     if (pattern > 14) {
         pattern = 0;
     }
-    sliceCanvasGivenPattern(ID_CANVAS_DEF_WEAP, pattern++);
+    sliceSpriteGivenPattern(defSpriteSettings, defWeaponCanvas, pattern++);
 };
 
 document.getElementById(ID_BTN_ZOOM_IN).onclick = () => {
@@ -340,14 +399,14 @@ document.getElementById(ID_BTN_CLEAR).onclick = () => {
 };
 
 document.getElementById(ID_BTN_START_MODIF).onclick = () => {
-    if (pattern > nbPatterns) {
+    if (pattern > defSpriteSettings.nbPatterns) {
         pattern = 1;
     }
-    sliceCanvasGivenPattern(ID_CANVAS_TMP, 0);
+    sliceSpriteGivenPattern(defSpriteSettings, tmpCanvas, 0);
     zoomInCanvas(ID_CANVAS_TMP, ID_CANVAS_DEF_WEAP);
     copyCanvas(ID_CANVAS_DEF_WEAP, ID_CANVAS_TMP);
 
-    sliceCanvasGivenPattern(ID_CANVAS_TMP, pattern++);
+    sliceSpriteGivenPattern(defSpriteSettings, tmpCanvas, pattern++);
     zoomInCanvas(ID_CANVAS_TMP, ID_CANVAS_MODIF_PATTERN);
     copyCanvas(ID_CANVAS_MODIF_PATTERN, ID_CANVAS_TMP);
 };
@@ -384,3 +443,5 @@ document.getElementById(ID_BTN_DARKEST).onclick = () => {
 document.getElementById(ID_BTN_ERASE).onclick = () => {
     currPixelColor.setEraseColor();
 };
+
+//#endregion
